f9dcef95ee00ea238d847f58f1ae3d8c75464337
Paulino <paulinomendes03@gmail.com>
2025-03-26 18:12:16 -0300

    feat: início do projeto

---
+++ src/test/java/com/cli/livro/LivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cli.livro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
