41d16a341beb8fc8069a8fcee5abcaae5da9f7e8
Paulino <paulinomendes03@gmail.com>
2025-03-27 17:01:39 -0300

    feat(LivroMenu): adiciona o menu com cadastro e exclusão de livros

--- src/main/java/com/cli/livro/LivroApplication.java
+++ src/main/java/com/cli/livro/LivroApplication.java
@@ -1,8 +1,11 @@
 package com.cli.livro;
 
 import com.cli.livro.persistence.migration.MigrationStrategy;
+import com.cli.livro.ui.LivroMenu;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -14,12 +17,19 @@ public class LivroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LivroApplication.class, args);
+	}
 
-		try (Connection connection = getConnection()) {
-			new MigrationStrategy(connection).executeMigration();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	@Bean
+	CommandLineRunner run() {
+		return args -> {
+			try (Connection connection = getConnection()) {
+				new MigrationStrategy(connection).executeMigration();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+			new LivroMenu().execute();
+		};
 	}
 
 }
---
+++ src/main/java/com/cli/livro/ui/LivroMenu.java
@@ -0,0 +1,93 @@
+package com.cli.livro.ui;
+
+import com.cli.livro.config.ConnectionConfig;
+import com.cli.livro.persistence.entity.LivroEntity;
+import com.cli.livro.service.LivroService;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class LivroMenu {
+
+    private final Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+
+    public void execute() throws SQLException {
+        System.out.println("\nBem-vindo ao gerenciador de livros, escolha a opção desejada");
+        while (true) {
+            System.out.println("\n+=============================+");
+            System.out.println("|        MENU DE LIVROS       |");
+            System.out.println("+=============================+");
+            System.out.println("| 1 - Cadastrar um novo livro |");
+            System.out.println("| 2 - Listar todos os livros  |");
+            System.out.println("| 3 - Buscar livro por título |");
+            System.out.println("| 4 - Excluir um livro        |");
+            System.out.println("| 5 - Sair                    |");
+            System.out.println("+=============================+");
+
+            inputCharacter();
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1 -> cadastrarLivro();
+                case 2 -> listarLivros();
+                case 3 -> buscarLivro();
+                case 4 -> excluirLivro();
+                case 5 -> System.exit(0);
+                default -> System.out.println("Opção inválida, informe uma opção do menu");
+            }
+        }
+    }
+
+    private void cadastrarLivro() throws SQLException {
+        System.out.println("\n>>>> CADASTRANDO UM NOVO LIVRO");
+
+        System.out.println("Informe o título do livro");
+        inputCharacter();
+        String titulo = scanner.next();
+
+        System.out.println("Informe o autor do livro");
+        inputCharacter();
+        String autor = scanner.next();
+
+        System.out.println("Informe o ano de publicação (opcional, pressione Enter para ignorar)");
+        inputCharacter();
+        String anoInput = scanner.next();
+        Integer anoPublicacao = anoInput.isEmpty() ? null : Integer.parseInt(anoInput);
+
+        var livro = new LivroEntity();
+        livro.setTitulo(titulo);
+        livro.setAutor(autor);
+        livro.setAnoPublicacao(anoPublicacao);
+
+        try (var connection = ConnectionConfig.getConnection()) {
+            var service = new LivroService(connection);
+            service.insert(livro);
+            System.out.println("Livro cadastrado com sucesso!");
+        }
+    }
+
+    private void listarLivros() throws SQLException {
+    }
+
+    private void buscarLivro() throws SQLException {
+    }
+
+    private void excluirLivro() throws SQLException {
+        System.out.println("\n>>>> EXCLUINDO UM LIVRO");
+        System.out.println("Informe o ID do livro que será excluído");
+        inputCharacter();
+        Long id = scanner.nextLong();
+
+        try (var connection = ConnectionConfig.getConnection()) {
+            var service = new LivroService(connection);
+            if (service.delete(id)) {
+                System.out.printf("O livro %s foi excluído com sucesso.\n", id);
+            } else {
+                System.out.printf("Não foi encontrado um livro com ID %s.\n", id);
+            }
+        }
+    }
+
+    private void inputCharacter() {
+        System.out.print(">> ");
+    }
+}
